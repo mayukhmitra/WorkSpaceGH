9e3567055900692acd725e1da4224dd58f4925df
mayukh <mayukh.mitra@trianz.com>
2017-04-11 12:54:13 +0530

    Making a change in a branch

--- B.java
+++ B.java
@@ -1,6 +1,16 @@
+class C {
+	void display() {
+		System.out.println("After pulling from central repo to local repo, edited code..");
+	}
+}
+
+
 class B {
 
-  public static void main(String args[]) {
-    System.out.println("It's new JAVA file created in Central repository");
-  }
- }
+  	public static void main(String args[]) {
+    		System.out.println("It's new JAVA file created in Central repository");
+		C obj = new C();
+		obj.disp();
+
+	}
+}
